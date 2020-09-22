5e9a14783a29fbcc7e9a77c016b6ee5ae92bd61a
MonikaSai <54144435+MonikaSai@users.noreply.github.com>
2020-09-22 20:01:24 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# colors
+Teal = (0, 128, 128)
+BLACK = (0, 0, 0)
+
+SCALE = 32
+
+SCREEN_HEIGHT = 480
+SCREEN_WIDTH = 640
---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+import config
+from game_state import GameState
+
+from game import Game
+
+pygame.init()
+
+screen = pygame.display.set_mode((config.SCREEN_WIDTH, config.SCREEN_HEIGHT))
+
+pygame.display.set_caption("Pokemon")
+
+clock = pygame.time.Clock()
+
+game = Game(screen)
+game.set_up()
+
+while game.game_state == GameState.RUNNING:
+    clock.tick(50)
+    game.update()
+    pygame.display.flip()
+
+
+
+
